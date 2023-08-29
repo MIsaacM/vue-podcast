6a54ce09e4232a095ef23626370cda310fcbd8b9
MIsaacMedina <MIsaacMedina@gmail.com>
2023-08-29 17:32:15 +0100

    config vite to build develop & production

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,35 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command, mode }) => {
+  const isProd = mode === 'production';
+  console.log('env: ', mode, command)
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      host: 'localhost',
+      port: 8080,
+      proxy: {
+        '/api': {
+          target: 'IP/URL',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
+    build: {
+      outDir: isProd ? 'dist' : 'dist-dev',
+      assetsDir: 'assets',
+      cssCodeSplit: false,
+      sourcemap: isProd,
+      minify: isProd,
     }
   }
-})
+});
