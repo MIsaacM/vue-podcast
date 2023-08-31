969039eda9c605c6c0aeb8df983abff4477fce23
MIsaacMedina <MIsaacMedina@gmail.com>
2023-08-31 20:59:54 +0100

    catched action errors in console

--- src/helpers/storeHelper.js
+++ src/helpers/storeHelper.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unsafe-finally */
 import { reactive } from 'vue';
 import { parse } from 'rss-to-json';
 import Axios from './axiosHelper';
@@ -5,61 +6,106 @@ import utils from '../utils/utils';
 
 const state = reactive({
   loadingRoute: false,
+  loadingData: false,
   podcastList: [],
   podcastDetailsList: {},
 });
 
 const actions = {
   async fetchPodcasts(limit = 100) {
-    const cachedPodcastList = utils.getFromCache('podcastList');
-    if (cachedPodcastList) {
-      state.podcastList = cachedPodcastList;
-    } else {
-      const axios = new Axios({ baseURL: 'https://itunes.apple.com' });
-      const podcastsResponse = await axios.instance.get(`us/rss/toppodcasts/limit=${limit}/genre=1310/json`);
-      const podcasts = podcastsResponse?.data?.feed?.entry || [];
-      utils.setInCache('podcastList', podcasts, 86400000);
-      state.podcastList = podcasts;
+    try {
+      state.loadingData = true;
+      const cachedPodcastList = utils.getFromCache('podcastList');
+      if (cachedPodcastList) {
+        state.podcastList = cachedPodcastList;
+      } else {
+        const axios = new Axios({ baseURL: 'https://itunes.apple.com' });
+        const podcastsResponse = await axios.instance.get(`us/rss/toppodcasts/limit=${limit}/genre=1310/json`);
+        const podcasts = podcastsResponse?.data?.feed?.entry || [];
+        utils.setInCache('podcastList', podcasts, 86400000);
+        state.podcastList = podcasts;
+      }
+    } catch (error) {
+      console.error({
+        message: 'Error on fetchPodcasts',
+        error,
+      });
+    } finally {
+      state.loadingData = false;
+      return state.podcastList;
     }
-    
-    return state.podcastList;
   },
   async fetchPodcast(podcastId) {
-    if (!podcastId) {
-      console.error('"podcastId" is mandatory to look for a podcast');
-      return {};
-    }
+    try {
+      if (!podcastId) {
+        console.error('"podcastId" is mandatory to look for a podcast');
+        return {};
+      }
 
-    const cachedPodcast = utils.getFromCache(`podcast-${podcastId}`);
-    if (cachedPodcast) {
-      state.podcastDetailsList[podcastId] = cachedPodcast;
-    } else {
-      const axios = new Axios({ baseURL: 'https://api.allorigins.win/raw?url=' });
-      const podcastResponse = await axios.instance.get(`https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcast`);
-      const podcast = podcastResponse?.data?.results?.length && podcastResponse.data.results[0] || [];
-      utils.setInCache(`podcast-${podcastId}`, podcast, 86400000);
-      state.podcastDetailsList[podcastId] = podcast;
+      state.loadingData = true;
+      const cachedPodcast = utils.getFromCache(`podcast-${podcastId}`);
+      if (cachedPodcast) {
+        state.podcastDetailsList[podcastId] = cachedPodcast;
+      } else {
+        const axios = new Axios({ baseURL: 'https://api.allorigins.win/raw?url=' });
+        const podcastResponse = await axios.instance.get(`https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcast`);
+        const podcast = podcastResponse?.data?.results?.length && podcastResponse.data.results[0] || [];
+        utils.setInCache(`podcast-${podcastId}`, podcast, 86400000);
+        state.podcastDetailsList[podcastId] = podcast;
+      }
+    } catch (error) {
+      console.error({
+        message: 'Error on fetchPodcast',
+        error,
+      });
+    } finally {
+      state.loadingData = false;
+      return state.podcastDetailsList[podcastId];
     }
-
-    return state.podcastDetailsList[podcastId];
   },
   async fetchPodcastEpisodes(podcastId, limit = 500) {
-    if (!podcastId) {
-      console.error('"podcastId" is mandatory to look for the list of podcast episodes');
-      return [];
+    let episodes = [];
+    try {
+      if (!podcastId) {
+        console.error('"podcastId" is mandatory to look for the list of podcast episodes');
+        return [];
+      }
+
+      state.loadingData = true;
+      const axios = new Axios({ baseURL: 'https://api.allorigins.win/raw?url=' });
+      const episodesResponse = await axios.instance.get(`https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=${limit}`);
+      episodes = episodesResponse?.data?.results?.length && episodesResponse?.data?.results || [];
+
+    } catch (error) {
+      console.error({
+        message: 'Error on fetchPodcastEpisodes',
+        error,
+      });
+    } finally {
+      state.loadingData = false;
+      return episodes;
     }
-    const axios = new Axios({ baseURL: 'https://api.allorigins.win/raw?url=' });
-    const episodesResponse = await axios.instance.get(`https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=${limit}`);
-    const episodes = episodesResponse?.data?.results?.length && episodesResponse?.data?.results || [];
-    return episodes;
   },
   async fetchFeed(url) {
-    if (!url) {
-      console.error('"url" is mandatory to get url feed');
-      return {};
+    let jsonFeed = {};
+    try {
+      if (!url) {
+        console.error('"url" is mandatory to get url feed');
+        return {};
+      }
+
+      state.loadingData = true;
+      jsonFeed = await parse(url);
+
+    } catch (error) {
+      console.error({
+        message: 'Error on fetchFeed',
+        error,
+      });
+    } finally {
+      state.loadingData = false;
+      return jsonFeed;
     }
-    const jsonFeed = await parse(url);
-    return jsonFeed;
   },
 };
 
